62e5c630aa4a31b59089e56b406018013bbd9f2d
Josh Biddick <josh@ricado.co.nz>
2020-04-05 13:40:55 +1200

    add route forand menu item for cards

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,112 +1,113 @@
 import React from "react";
 import {
-  Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Image,
-  List,
-  Menu,
-  Segment,
+    Container,
+    Divider,
+    Dropdown,
+    Grid,
+    Header,
+    Image,
+    List,
+    Menu,
+    Segment,
 } from "semantic-ui-react";
+import Link from "next/link";
 
 const Layout: React.FC<any> = (props) => (
-  <div>
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item as="a" header>
-          <Image size="mini" src="/images/logo.png" style={{ marginRight: "1.5em" }} />
-          Project Name
-        </Menu.Item>
-        <Menu.Item as="a">Home</Menu.Item>
+    <div>
+        <Menu fixed="top" inverted>
+            <Container>
+                <Menu.Item as="a" header>
+                    <Image
+                        size="mini"
+                        src="/images/logo.png"
+                        style={{ marginRight: "1.5em" }}
+                    />
+                    Nachotime!
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/">
+                        <a>Home</a>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/exercises">
+                        <a>Exercises</a>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/cards">
+                        <a>Cards</a>
+                    </Link>
+                </Menu.Item>
+            </Container>
+        </Menu>
 
-        <Dropdown item simple text="Dropdown">
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className="dropdown icon" />
-              <span className="text">Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
+        <Container text style={{ marginTop: "7em" }}>
+            {props.children}
+        </Container>
 
-    <Container text style={{ marginTop: "7em" }}>
-      {props.children}
-    </Container>
+        <Segment
+            inverted
+            vertical
+            style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
+        >
+            <Container textAlign="center">
+                <Grid divided inverted stackable>
+                    <Grid.Column width={3}>
+                        <Header inverted as="h4" content="Group 1" />
+                        <List link inverted>
+                            <List.Item as="a">Link One</List.Item>
+                            <List.Item as="a">Link Two</List.Item>
+                            <List.Item as="a">Link Three</List.Item>
+                            <List.Item as="a">Link Four</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                        <Header inverted as="h4" content="Group 2" />
+                        <List link inverted>
+                            <List.Item as="a">Link One</List.Item>
+                            <List.Item as="a">Link Two</List.Item>
+                            <List.Item as="a">Link Three</List.Item>
+                            <List.Item as="a">Link Four</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column width={3}>
+                        <Header inverted as="h4" content="Group 3" />
+                        <List link inverted>
+                            <List.Item as="a">Link One</List.Item>
+                            <List.Item as="a">Link Two</List.Item>
+                            <List.Item as="a">Link Three</List.Item>
+                            <List.Item as="a">Link Four</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column width={7}>
+                        <Header inverted as="h4" content="Footer Header" />
+                        <p>
+                            Extra space for a call to action inside the footer
+                            that could help re-engage users.
+                        </p>
+                    </Grid.Column>
+                </Grid>
 
-    <Segment
-      inverted
-      vertical
-      style={{ margin: "5em 0em 0em", padding: "5em 0em" }}
-    >
-      <Container textAlign="center">
-        <Grid divided inverted stackable>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Group 1" />
-            <List link inverted>
-              <List.Item as="a">Link One</List.Item>
-              <List.Item as="a">Link Two</List.Item>
-              <List.Item as="a">Link Three</List.Item>
-              <List.Item as="a">Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Group 2" />
-            <List link inverted>
-              <List.Item as="a">Link One</List.Item>
-              <List.Item as="a">Link Two</List.Item>
-              <List.Item as="a">Link Three</List.Item>
-              <List.Item as="a">Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Header inverted as="h4" content="Group 3" />
-            <List link inverted>
-              <List.Item as="a">Link One</List.Item>
-              <List.Item as="a">Link Two</List.Item>
-              <List.Item as="a">Link Three</List.Item>
-              <List.Item as="a">Link Four</List.Item>
-            </List>
-          </Grid.Column>
-          <Grid.Column width={7}>
-            <Header inverted as="h4" content="Footer Header" />
-            <p>
-              Extra space for a call to action inside the footer that could help
-              re-engage users.
-            </p>
-          </Grid.Column>
-        </Grid>
-
-        <Divider inverted section />
-        <Image centered size="mini" src="/images/logo.png" />
-        <List horizontal inverted divided link size="small">
-          <List.Item as="a" href="#">
-            Site Map
-          </List.Item>
-          <List.Item as="a" href="#">
-            Contact Us
-          </List.Item>
-          <List.Item as="a" href="#">
-            Terms and Conditions
-          </List.Item>
-          <List.Item as="a" href="#">
-            Privacy Policy
-          </List.Item>
-        </List>
-      </Container>
-    </Segment>
-  </div>
+                <Divider inverted section />
+                <Image centered size="mini" src="/images/logo.png" />
+                <List horizontal inverted divided link size="small">
+                    <List.Item as="a" href="#">
+                        Site Map
+                    </List.Item>
+                    <List.Item as="a" href="#">
+                        Contact Us
+                    </List.Item>
+                    <List.Item as="a" href="#">
+                        Terms and Conditions
+                    </List.Item>
+                    <List.Item as="a" href="#">
+                        Privacy Policy
+                    </List.Item>
+                </List>
+            </Container>
+        </Segment>
+    </div>
 );
 
 export default Layout;
---
+++ pages/cards.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const Cards: NextPage<any> = () => (
+  <h1>Cards!</h1>
+);
+
+export default Cards;
