e1329602a88569b4d9f062f522342a0087514368
Josh Biddick <josh@ricado.co.nz>
2020-05-09 22:20:55 +1200

    Add dark mode button

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,8 +1,11 @@
 import React from "react";
-import { Container, Image, Menu } from "semantic-ui-react";
+import { Container, Image, Menu, Checkbox, Icon } from "semantic-ui-react";
 import styled from "styled-components";
+import { useRouter } from "next/router";
 
 const Layout: React.FC<any> = (props) => {
+    const { query } = useRouter();
+    const darkMode = query.darkMode && JSON.parse(query.darkMode as string);
     return (
         <>
             <StyledMenu borderless>
@@ -11,6 +14,17 @@ const Layout: React.FC<any> = (props) => {
                         <StyledLogo size="mini" src="/images/logo.png" />
                         Nachotime!
                     </Menu.Item>
+                    {darkMode ? (
+                        <Menu.Item position="right" href="/">
+                            <Icon name="sun outline" />
+                            Light Mode
+                        </Menu.Item>
+                    ) : (
+                        <Menu.Item position="right" href="/?darkMode=true">
+                            <Icon name="moon" />
+                            Dark Mode
+                        </Menu.Item>
+                    )}
                 </Container>
             </StyledMenu>
 
